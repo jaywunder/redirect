447f1dc6a36a8f34f77cb3f4bab316b485c7f37b
jaywunder <jaywunder@gmail.com>
2016-04-07 19:04:15 -0400

    added a way to know whether something is distracting or not and added a local schema for distraction storage

--- src/content_script.js
+++ src/content_script.js
@@ -1,18 +1,66 @@
 'use strict'
+// change to using a schema eventually
+let defaultConfig = {
+  distractions: [
+    'youtube.com', 'news.ycombinator.com', 'reddit.com', 'tumblr.com',
+    'facebook.com', 'messenger.com', 'twitter.com'
+  ]
+}
 
-var popup = `
-  <div class="ext-redirect-outer">
-    <div class="ext-redirect-inner">
-      <h1>HELLO THERE IM IN THE MIDDLE OF EVERYTHING ARENT I???</h1>
+let config = {};
+
+chrome.storage.sync.get({
+    distractions: defaultConfig.distractions
+  },
+  (data) => {
+    config.distractions = data.distractions
+    console.log(data);
+    main(config)
+  }
+)
+
+// chrome.storage.sync.get(["distractions"], (data) => {
+//   config.distractions = data.distractions
+//   console.log(data);
+//   main(data)
+// })
+
+
+function main(config) {
+  if (isDistracting(location.hostname)) {
+    redirect()
+    createUI()
+  }
+}
+
+function isDistracting(url) {
+  for(let i in config.distractions) {
+    if (url.includes(config.distractions[i])) return true
+  }
+  return false
+}
+
+function redirect() {
+  console.log('this website is a distraction!!');
+}
+
+function createUI() {
+  // "ext-redirect-" means "extension redirect"
+  // I don't want any name conlicts with other websites
+  var popup = `
+    <div class="ext-redirect-outer">
+      <div class="ext-redirect-inner">
+        <h1>Don't get distracted!</h1>
+      </div>
     </div>
-  </div>
-`
-$('body').append($(popup))
+  `
+  $('body').append($(popup))
 
-let $outer = $('.ext-redirect-outer')
-let $inner = $('.ext-redirect-inner')
+  let $outer = $('.ext-redirect-outer')
+  let $inner = $('.ext-redirect-inner')
 
-$inner.css({
-  left: (window.innerWidth / 2 - $inner.width() / 2) + 'px',
-  top: (window.innerHeight / 2 - $inner.height() / 2) + 'px'
-})
+  $inner.css({
+    left: (window.innerWidth / 2 - $inner.width() / 2) + 'px',
+    top: (window.innerHeight / 2 - $inner.height() / 2) + 'px'
+  })
+}
